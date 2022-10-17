a930ab8c2b89675f197fb77a5ac0a1e62741712d
Hitray <75577433+Hitray@users.noreply.github.com>
2022-10-17 07:56:55 +0500

    Update Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,94 +1,99 @@
-import java.util.Objects;
-import java.util.OptionalInt;
-
 public class Person {
-    protected final String name;
-    protected final String surname;
-    protected int age;
-    protected String address;
+    private final String name;
+    private final String surName;
+    private int age;
+    private String city;
 
-    public Person(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
+  
+    private Person(Builder builder) {
+        this.name = builder.name;
+        this.surName = builder.surName;
+        this.age = builder.age;
+        this.city = builder.city;
     }
+  
+    public String getName() {return name;}
+    public String getSurName() {return surName;}
+    public int getAge() {return age;}
+    public String getAddress() {return city;}
 
-    public Person(String name, String surname, int age) {
-        this.name = name;
-        this.surname = surname;
+
+    public boolean hasAge() {return this.age >= 0;}
+    public boolean hasAddress() {return !(this.city == null);}
+
+    
+    public void setAge(int age) {
+        if (hasAge()) {
+            throw new IllegalStateException("Возраст уже установлен, дальше через happyBirthday()");
+        }
+        if (age < 0) {
+            throw new IllegalArgumentException("Некорректное значение возраста");
+        }
         this.age = age;
     }
-
-    public boolean hasAge() {
-        if (age > 0) {
-            return true;
-        } else
-            return false;
-    }
-
-    public boolean hasAddress() {
-        if (address != null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public OptionalInt getAge() {
-        return OptionalInt.of(age);
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public void happyBirthday() {
-        if (hasAge()) {
-            age++;
-        }
+        if (hasAge()) {this.age++;}
     }
+    //
+    public void setAddress(String city) {this.city = city;}
+    //
+
 
     @Override
     public String toString() {
-        return  "имя - " + name + "\n" +
-                "фамилия - " + surname + "\n" +
-                "возраст - " + age + "\n" +
-                "место жительства - " + address + "\n";
+        return name + ' ' +
+                surName +
+                " (возраст: " + age + ')';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
+   
+    public static class Builder {
+        // Необходимые параметры
+        private String name;
+        private String surName;
+        // Необязательные параметры
+        private int age = -1;
+        private String city = null;
+
+        public Builder() {}
+
+        public Builder setName(String name) {
+            if (this.name != null) {throw new IllegalStateException("Имя уже есть\n");}
+            this.name = name;
+            return this;
         }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
+
+        public Builder setSurName(String surName) {
+            if (this.surName != null) {throw new IllegalStateException("Фамилия уже есть\n");}
+            this.surName = surName;
+            return this;
         }
-        Person person = (Person) o;
-        return age == person.age
-                && name.equals(person.name)
-                && surname.equals(person.surname)
-                && Objects.equals(address, person.address);
+
+        public Builder setAge(int age) {
+            if (this.age >= 0) {throw new IllegalStateException("Возраст уже введен\n");}
+            if (age < 0) {throw new IllegalArgumentException("Возраст меньше нуля - рили?\n");}
+            this.age = age;
+            return this;
+        }
+
+        public Builder setAddress(String city) {
+            if (this.city != null) {throw new IllegalStateException("Город уже задан\n");}
+            this.city = city;
+            return this;
+        }
+
+        public Person build() {
+            if (this.name == null || this.surName == null) {
+                throw new IllegalStateException("Имя и Фамилия обязательны\n");
+            }
+            return new Person(this);
+        }
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, surname, age, address);
-    }
-
-
-    public PersonBuilder newChildBuilder() {
-        return new PersonBuilder().setSurname(surname).setAddress(address);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public String getAddress() {
-        return address;
+    public Builder newChildBuilder() {
+        var child = new Builder();
+        child.setSurName(this.surName);
+        child.setAddress(this.city);
+        child.setAge(0);
+        return child;
     }
 }
