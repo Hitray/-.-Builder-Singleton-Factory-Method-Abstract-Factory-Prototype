5e98585e7b00c9c2b370ade64e0e0d2ed284c46a
Hitray <75577433+Hitray@users.noreply.github.com>
2022-10-17 07:42:29 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,39 @@
+
 public class Main {
     public static void main(String[] args) {
-
-        Person mom = new Person.Builder()
+        Person mom = new PersonBuilder()
                 .setName("Анна")
-                .setSurName("Вольф")
-                .setAge(21)
+                .setSurname("Вольф")
+                .setAge(31)
                 .setAddress("Сидней")
                 .build();
         Person son = mom.newChildBuilder()
                 .setName("Антошка")
                 .build();
-        System.out.println("У " + mom + " есть сын, " + son);
+        System.out.println("у " + mom + " есть сын, " + "\n" + son);
+
+        try {
+            Person dad = new PersonBuilder()
+                    .setName("Пётр")
+                    .setAge(38)
+                    .build();
+            
+//            new PersonBuilder().build();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Person grandmother = new PersonBuilder()
+                    .setName("Галина")
+                    .setSurname("Иванова")
+                    .setAge(-100)
+                    .build();
+            
+//            new PersonBuilder().setAge(-100).build();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
     }
 }
